b3cdeb68533a8c97da1dfbd39280550ac8f783c1
mateorey22 <149890580+mateorey22@users.noreply.github.com>
2025-03-02 21:12:32 +0100

    Add files via upload

--- api.py
+++ api.py
@@ -1,1 +1,28 @@
- 
+from flask import Flask, jsonify
+from flask_cors import CORS
+import psutil
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/api/test', methods=['GET'])
+def test_api():
+    return jsonify({'message': 'API is working'})
+
+@app.route('/api/system/info', methods=['GET'])
+def get_system_info():
+    cpu_temperature = os.popen("vcgencmd measure_temp").readline().replace("temp=","").replace("'C\\n","")
+    ram_usage = psutil.virtual_memory().percent
+    load_system = psutil.getloadavg()[0]
+    disk_space = psutil.disk_usage('/').percent
+    
+    return jsonify({
+        'cpu_temperature': cpu_temperature,
+        'ram_usage': ram_usage,
+        'load_system': load_system,
+        'disk_space': disk_space
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
