fd6132992a12c67c3b084a99c1b8d363a2bb060b
mateorey22 <149890580+mateorey22@users.noreply.github.com>
2025-03-02 21:38:15 +0100

    Add files via upload

--- api.py
+++ api.py
@@ -2,9 +2,23 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 import psutil
 import os
+import subprocess
 
 app = Flask(__name__)
 CORS(app)
+
+def is_camera_connected():
+    try:
+        # Run v4l2-ctl command to list devices
+        result = subprocess.run(['v4l2-ctl', '--list-devices'], capture_output=True, text=True, check=True)
+        # Check if any device is listed
+        return "Cannot open device" not in result.stdout
+    except subprocess.CalledProcessError:
+        return False
+    except FileNotFoundError:
+        # v4l2-ctl is not installed
+        return False
+
 
 @app.route('/api/test', methods=['GET'])
 def test_api():
@@ -23,6 +37,11 @@ def get_system_info():
         'load_system': load_system,
         'disk_space': disk_space
     })
+
+@app.route('/api/camera/status', methods=['GET'])
+def get_camera_status():
+    status = is_camera_connected()
+    return jsonify({'connected': status})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
